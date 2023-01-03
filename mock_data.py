36d44529abf49aa414cad983a9ca166da1bc57f9
Manuelbailey101 <115514197+Manuelbailey101@users.noreply.github.com>
2023-01-02 19:52:21 -0800

    108 #3

---
+++ mock_data.py
@@ -0,0 +1,26 @@
+catalog = [
+    {
+        "title": "apples",
+        "price": 2.99,
+        "Category": "fruit",
+        "image": "apple.jpg"
+    },
+    {
+        "title": "cabbage",
+        "price": 3.99,
+        "Category": "vegatable",
+        "image": "cabbage.jpg"
+    },
+    {
+        "title": "bell pepper",
+        "price": 6,
+        "Category": "vegatable",
+        "image": "bell.jpg"
+    },
+    {
+        "title": "banana",
+        "price": 6.99,
+        "Category": "fruit",
+        "image": "banana.jpg"
+    }
+]
