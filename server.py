54ad2df0ff83ac450cdaa5eec6e80508aab09cd0
Manuelbailey101 <115514197+Manuelbailey101@users.noreply.github.com>
2023-01-04 19:47:27 -0800

    python assign 4

--- server.py
+++ server.py
@@ -30,9 +30,57 @@ def version():
     } 
     return json.dumps(version) 
 
+
 @app.get("/api/catalog")
 def get_catalog():
     return json.dumps(catalog)
+
+@app.get("/api/catalog/<category>")
+def get_by_category(category):
+    result = []
+    for prod in catalog:
+        if prod["Category"].lower() == category.lower():
+            result.append(prod)
+
+    return json.dumps(result) 
+
+
+@app.get("/api/catalog/search/search/<title>")
+def search_by_title(title):
+    result = []
+    for prod in catalog:
+        if title.lower() in prod["title"].lower():
+            result.append(prod)  
+    return json.dumps(result)            
+
+
+
+
+@app.get('/api/product/cheaper/<price>')
+def search_by_price(price):
+    result = []
+    for prod in catalog:
+        if prod ["price"] < float (price):
+           result.append(prod)
+
+    return json.dumps(result)     
+
+
+@app.get("/api/product/count")
+def count_product():
+    count = len(catalog)
+    return json.dumps(count)
+
+
+
+@app.get("/api/product/cheapest")
+def get_cheapest():
+    answer = catalog[0]
+    for prod in catalog:
+        if prod ["price"]< answer["answer"]:
+            answer = prod     
+
+    return json.dumps(answer)
 
 
 @app.get('/test/numbers')
